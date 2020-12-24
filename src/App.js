ee596cdcb8d0b558a168b4c56f9bbda2cfc48904
oakleyHD <oakleycoldcube@gmail.com>
2020-12-24 12:41:02 +0300

    Update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import Header from './components/Header/Header';
-import Main from './components/Header/Main/Main';
+import Main from './components/Main/Main';
 import NavBar from './components/NavBar/NavBar';
 
 function App() {
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,11 +3,11 @@ import classes from "./NavBar.module.css";
 const NavBar = ()=> {
     return(
         <nav className={`bg_container ${classes.nav}`}>
-         <ul>
-          <li>Messages</li>
-          <li>Friends</li>
-          <li>Settings</li>
-          <li>Shop</li>
+         <ul className={classes.panel}>
+          <li className={classes.panelItems}><a href="#">Messages</a></li>
+          <li className={classes.panelItems}><a href="#">Friends</a></li>
+          <li className={classes.panelItems}><a href="#">Settings</a></li>
+          <li className={classes.panelItems}><a href="#">Shop</a></li>
         </ul>
         </nav>
     )
